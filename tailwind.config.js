91d54cbd715c7a014d81b59c8d0d320f9102af65
firomsa <firomsassf@gmail.com>
2024-06-06 07:45:09 +0300

    initializing

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'orange-light':'#fff6f6',
+      'orange':'#FF2626'
+    }
+  },
+  plugins: [],
+}
+
